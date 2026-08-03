95f3bebab6c76395b9c155b040227f5ee141468e
rishi alluri <rishi.alluri@colibrigroup.com>
2026-08-03 21:18:44 +0530

    chore : update local host 4200 to 4003 for admin portal

--- apps/public-portal/src/environments/environment.local.ts
+++ apps/public-portal/src/environments/environment.local.ts
@@ -9,6 +9,6 @@ export const environment: AppEnvironment = {
     apexDomain: 'lvh.me',
     port: 4200,
     mainHosts: ['localhost', 'ssrkeducation.lvh.me'],
-    adminPortalUrl: 'http://localhost:4201',
+    adminPortalUrl: 'http://localhost:4003',
   },
 };
